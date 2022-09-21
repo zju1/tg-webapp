d8ed4757ba58fe33a29886ad6f0168edad170b6b
singledeveloper63 <singledeveloper63@gmail.com>
2022-09-21 18:21:29 +0500

    Add MainButton for tg

--- src/app/appSlice.js
+++ src/app/appSlice.js
@@ -2,7 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cart: [],
-  total: 0,
 };
 
 const appSlice = createSlice({
--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,8 +1,32 @@
 import "./home.css";
 import data from "../../db/MOCK_DATA.json";
 import Card from "./Card";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import useWebApp from "../../hooks/useWebApp";
 
 export default function Home() {
+  const cart = useSelector((store) => store.cart);
+
+  const total = cart.reduce((prev, current) => {
+    return (prev += current.count * current.product.price);
+  }, 0);
+
+  const webApp = useWebApp();
+
+  useEffect(() => {
+    if (total > 0) {
+      webApp.MainButton.setText(`Satıp alıw: ${total.toLocaleString()} UZS`);
+      if (!webApp.MainButton.isVisible) {
+        webApp.MainButton.show();
+      }
+    } else {
+      if (webApp.MainButton.isVisible) {
+        webApp.MainButton.hide();
+      }
+    }
+  }, [cart, total, webApp]);
+
   return (
     <div className="container">
       <div className="products">
